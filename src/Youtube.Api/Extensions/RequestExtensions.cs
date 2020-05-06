3b450ff6163a8d410ceb2429f302c4421a4a052a
yungKillaGit <59793907+yungKillaGit@users.noreply.github.com>
2020-05-06 11:41:59 +0300

    add request extensions

---
+++ src/Youtube.Api/Extensions/RequestExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Youtube.Api.Extensions
+{
+    public static class RequestExtensions
+    {
+        public static string GetHeader(this HttpRequest request, string key)
+        {
+            return request.Headers.FirstOrDefault(x => x.Key == key).Value.FirstOrDefault();
+        }
+    }
+}
